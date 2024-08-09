67a2495ee2799f4434716c862b6f09d4768bdfdf
rkapl123 <rolandk@kapl.org>
2024-08-09 19:52:50 +0200

    fixed deploy problem

--- Distribution/deployAddin.cmd
+++ Distribution/deployAddin.cmd
@@ -2,13 +2,13 @@ rem copy Addin and settings...
 @echo off
 if exist "C:\Program Files\Microsoft Office\root\" (
 	echo 64bit office
-	copy /Y Raddin64.xll "%appdata%\Microsoft\AddIns\Raddin.xll"
-	copy /Y Raddin.xll.config "%appdata%\Microsoft\AddIns\Raddin.xll.config"
+	copy /Y ScriptAddin64.xll "%appdata%\Microsoft\AddIns\ScriptAddin.xll"
+	copy /Y ScriptAddin.xll.config "%appdata%\Microsoft\AddIns\ScriptAddin.xll.config"
 ) else (
 	echo 32bit office
-	copy /Y Raddin32.xll "%appdata%\Microsoft\AddIns\Raddin.xll"
-	copy /Y Raddin.xll.config "%appdata%\Microsoft\AddIns\Raddin.xll.config"
+	copy /Y ScriptAddin32.xll "%appdata%\Microsoft\AddIns\ScriptAddin.xll"
+	copy /Y ScriptAddin.xll.config "%appdata%\Microsoft\AddIns\ScriptAddin.xll.config"
 )
 rem start Excel and install Addin there..
-cscript //nologo installRAddinInExcel.vbs
+cscript //nologo installAddinInExcel.vbs
 pause
